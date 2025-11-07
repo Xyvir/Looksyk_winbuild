1d4e57d01463c3faaf2262fe7fa9b6cbd350b0d6
Dunko Xyvir <tymills620@gmail.com>
2025-11-07 17:05:26 -0600

    Update index.ts

--- application-wrapper/Looksyk/src/index.ts
+++ application-wrapper/Looksyk/src/index.ts
@@ -1,13 +1,13 @@
 import { spawn } from 'child_process';
 import { BehaviorSubject, filter, firstValueFrom, identity } from "rxjs";
 import { ArgumentConfig, parse } from "ts-command-line-args";
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
 
 const { app, BrowserWindow, globalShortcut, shell } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
 
 const gotTheLock = app.requestSingleInstanceLock();
 
